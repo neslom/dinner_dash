6f243727dcbd1db07dc31976835193916bea1a5a
Markus Olsen <molsen13@gmail.com>
2015-03-10 15:09:34 -0600

    Visitor can navigate to Items index page by clicking Browse All Items link.

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -2,20 +2,28 @@ require 'rails_helper'
 
 RSpec.describe "Home page" do
   context "as an unauthenticated user" do
-    it "shows a link to browse all items" do
-      visit root_path
+    let!(:category_1) { create(:category) }
+    let!(:category_2) { create(:category, name: "side item") }
+    let!(:item) { create(:item, name: "Avocado Toast") }
 
+    before(:each) { visit root_path }
+
+    it "shows a link to browse all items" do
       expect(page).to have_link("Browse All Items", href: items_path)
     end
 
     it "shows a link to browse by category" do
-      category = create(:category)
-
-      visit root_path
-
-      expect(page).to have_link("Browse By Category: #{category.name}", href: category_path(category))
+      expect(page).to have_link("Browse By Category: #{category_1.name}", href: category_path(category_1))
+      expect(page).to have_link("Browse By Category: #{category_2.name}", href: category_path(category_2))
     end
 
+    it "shows a list of all items once Browse All Items link is clicked" do
+      find_link("Browse All Items", href: items_path).click
+
+      expect(current_path).to eq(items_path)
+
+      expect(page).to have_link("Avocado Toast")
+    end
 
   end
 end
