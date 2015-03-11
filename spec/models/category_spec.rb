d53cc6c04599f3cf0c46da4fd35ec948aa833c6e
Kyra Weber <kyra.m.weber@gmail.com>
2015-03-11 13:44:54 -0600

    Added a user model test, and added rake command to rakefile

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -6,5 +6,10 @@ RSpec.describe Category, type: :model do
   it "has valid attributes" do
     expect(category.name).to eq("side dishes")
   end
-  
+
+  it "is not valid without a name" do
+    other_category = Category.create(name:nil)
+    expect(other_category).not_to be_valid
+  end
+
 end
