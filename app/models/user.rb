b06e77403782428ee40a6770f2a5fd72587d07c7
Markus Olsen <molsen13@gmail.com>
2015-03-10 22:16:27 -0600

    Testing basic User model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
+  validates :username, presence: true
+  validates :email_address, presence: true
+  validates :email_address, uniqueness: true
+  validates :display_name, length: { in: 2..32 }, allow_blank: true
+  validates_format_of :email_address, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/, on: :create
   has_secure_password
 end
