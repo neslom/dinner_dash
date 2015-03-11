fb259e9206759948de5efaad71c13aa99c32df85
Markus Olsen <molsen13@gmail.com>
2015-03-11 17:17:49 -0600

    Clean up Order History view test and move current_user stubbing to support helper method

--- spec/features/user_profile_spec.rb
+++ spec/features/user_profile_spec.rb
@@ -2,12 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "Authenticated User profile" do
   let!(:user) { create(:user) }
-  let(:order) { create(:order, user_id: user.id) }
-  #before(:each) { login_as(user) }
+  before(:each) { set_current_user(user) }
 
   it "can click Order History link to be taken to Order History page" do
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-
     visit root_path
 
     expect(page).to have_link("Order History")
@@ -20,11 +17,7 @@ RSpec.describe "Authenticated User profile" do
   end
 
   it "sees Order History" do
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-
     user.orders.create(cart: {"1" => 1})
-
-    #create(:order, user_id: 1)
 
     create(:item, id: 1)
 
--- spec/support/user_stubbing.rb
+++ spec/support/user_stubbing.rb
@@ -9,3 +9,7 @@ def login_as(user)
     click_link_or_button "Log in"
   end
 end
+
+def set_current_user(user)
+  allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+end
