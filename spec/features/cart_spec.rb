7a7b5b08ac79b4564e1dea1250b616f7ef048431
Markus Olsen <molsen13@gmail.com>
2015-03-11 22:38:24 -0600

    Cart page shows list of cart items

--- spec/features/cart_spec.rb
+++ spec/features/cart_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe "Cart" do
+  before do
+    create(:item)
+
+    visit items_path
+
+    click_link_or_button("Add to Cart")
+  end
+
+  it "shows a list of cart items" do
+    #cart = build(:cart)
+
+    visit cart_path
+
+    expect(page).to have_content("Cheese Toast")
+  end
 
 end
