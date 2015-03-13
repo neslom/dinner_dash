2dcd95107eb550d9a2e96930656e0e165d680e37
Edward McCarthy <efmccarthy26@gmail.com>
2015-03-13 17:44:41 -0600

    changed tests. removed different class names and made tests match the code

--- spec/support/user_stubbing.rb
+++ spec/support/user_stubbing.rb
@@ -5,9 +5,7 @@ def login_as(user)
   fill_in "session[email_address]", with: user.email_address
   fill_in "session[password]", with: user.password
 
-  within(".login_form") do
-    click_link_or_button "Log in"
-  end
+  click_link_or_button "Check In"
 end
 
 def set_current_user(user)
