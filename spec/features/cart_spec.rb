2545d65f88912126f475961ee7eb411ab6008ba3
Kyra Weber <kyra.m.weber@gmail.com>
2015-03-12 20:06:44 -0600

    intial branch commit

--- spec/features/cart_spec.rb
+++ spec/features/cart_spec.rb
@@ -35,4 +35,19 @@ RSpec.describe "Cart" do
     end
   end
 
+  it "can checkout cart" do
+    #     I can review my cart contents,
+    # and I click submit
+    # I can know see that order,
+    # in my order history,
+    # and that order has a status of ordered.
+
+    #visit page
+    #click checkout button
+    #see confirmation message
+    #visit order history page
+    #expect to see that order with status of ordered
+    
+  end
+
 end
