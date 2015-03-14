6189b183040b8ba545f9878799430a102c8db60c
Kyra Weber <kyra.m.weber@gmail.com>
2015-03-14 12:22:23 -0600

    Used rails method to get distance of time in words

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,11 +3,6 @@ class Order < ActiveRecord::Base
 
   belongs_to :user
   enum status: %w(ordered paid completed cancelled)
-  before_create :format_time
-
-  def format_time
-    updated_at.to_formatted_s(:long)
-  end
 
   def line_item_total(item, quantity)
     format_currency(item.price * quantity)
--- db/migrate/20150312234808_add_precision_to_item_price.rb
+++
@@ -1,6 +0,0 @@
-class AddPrecisionToItemPrice < ActiveRecord::Migration
-  def change
-    remove_column :items, :price
-    add_column :items, :price, :float, precision: 2
-  end
-end
