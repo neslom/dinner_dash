7ed6b001697de4c7a18bd3493ef31f7563f2b063
Markus Olsen <molsen13@gmail.com>
2015-03-13 19:20:20 -0600

    Move login location in nav bar and remove unnecessary nav bar rendering from item index

--- spec/features/admin_dashboard_spec.rb
+++ spec/features/admin_dashboard_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "Admin dashboard" do
-  xit "is directed to admin dashboard when admin logs in" do
+  it "is directed to admin dashboard when admin logs in" do
     admin = create(:admin)
     login_as(admin)
 
     expect(current_path).to eq(admin_path)
-
+    expect(page).to have_content("Admin")
+    expect(page).to have_content("Successfully logged in as #{admin.username}")
   end
 end
