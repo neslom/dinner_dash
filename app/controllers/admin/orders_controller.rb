e1e792371e693305983c0bebd2c1578e2164db78
Kyra Weber <kyra.m.weber@gmail.com>
2015-03-15 15:15:39 -0600

    Implemted manage orders dashboard for admin. Ability to filter orders by status

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,6 +2,15 @@ class Admin::OrdersController < ApplicationController
   layout "admin"
 
   def index
-    @orders = 
+    @orders = Order.all
+    @orders = Order.ordered if params[:status] == '0'
+    @orders = Order.paid if params[:status] == '1'
+    @orders = Order.completed if params[:status] == '2'
+    @orders = Order.cancelled if params[:status] == '3'
   end
+
+  def show
+    @order = Order.find(params[:id])
+  end
+
 end
--- spec/features/admin_order_management_spec.rb
+++ spec/features/admin_order_management_spec.rb
@@ -1,8 +1,35 @@
 require 'rails_helper'
 
 describe "Admin Order Managment" do
-  it "shows all orders" do
+  let(:admin) { create(:admin) }
+  let(:user) { create(:user) }
+  let!(:item) { create(:item, id: 1) }
+  let!(:order) { user.orders.create(cart: {"1"=> 2 } ) }
+
+  before(:each) do
+    login_as(admin)
     visit admin_orders_path
-    expect(page).to have_content("")
+  end
+
+  it "shows all orders" do
+    expect(page).to have_content("Manage all orders")
+    expect(page).to have_content(order.id)
+  end
+
+  it "links to a single order view" do
+    click_link_or_button(order.id.to_s)
+    expect(page).to have_content(order.user.username)
+  end
+
+  it "shows all items and their information" do
+    click_link_or_button(order.id.to_s)
+    expect(page).to have_link("Cheese Toast")
+    expect(page).to have_content("ordered")
+    expect(page).to have_content(order.created_at)
+    expect(page).to have_content(order.total)
+  end
+
+  it "shows total number of orders by status" do
+    expect(page).to have_content("Ordered: 1")
   end
 end
