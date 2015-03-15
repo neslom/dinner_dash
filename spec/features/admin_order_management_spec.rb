7ee5d086cfeb7f9809030ce793332297eb7e87f3
Markus Olsen <molsen13@gmail.com>
2015-03-15 16:17:51 -0600

    Shows accurate number of orders by status

--- spec/features/admin_order_management_spec.rb
+++ spec/features/admin_order_management_spec.rb
@@ -1,36 +1,57 @@
 require 'rails_helper'
 
-describe "Admin Order Managment" do
+describe "Admin Order Management" do
   let(:admin) { create(:admin) }
   let(:user) { create(:user) }
   let!(:item) { create(:item, id: 1) }
-  let!(:order) { user.orders.create(cart: {"1"=> 2 } ) }
+  let!(:order) { user.orders.create(cart: {"1"=> 2 }) }
 
-  before(:each) do
-    login_as(admin)
-    visit admin_orders_path
+  context "when on All Orders page" do
+
+    before(:each) do
+      login_as(admin)
+      visit admin_orders_path
+    end
+
+    it "shows all orders" do
+      expect(page).to have_content("Manage Orders")
+      expect(page).to have_content(order.id)
+    end
+
+    it "links to a single order view" do
+      click_link_or_button(order.id.to_s)
+      expect(page).to have_content(order.user.username)
+    end
+
+    it "shows all items and their information" do
+      click_link_or_button(order.id.to_s)
+      expect(page).to have_link("Cheese Toast")
+      expect(page).to have_content("ordered")
+      expect(page).to have_content(order.created_at)
+      expect(page).to have_content(order.total)
+    end
+
   end
 
-  it "shows all orders" do
-    expect(page).to have_content("Manage Orders")
-    expect(page).to have_content(order.id)
-  end
+  context "when on the Admin Order Dashboard" do
 
-  it "links to a single order view" do
-    click_link_or_button(order.id.to_s)
-    expect(page).to have_content(order.user.username)
-  end
+    before(:each) do
+      login_as(admin)
+      user.orders.create(status:1, cart: {"1" => 3} )
+      user.orders.create(status:1, cart: {"1" => 3} )
 
-  it "shows all items and their information" do
-    click_link_or_button(order.id.to_s)
-    expect(page).to have_link("Cheese Toast")
-    expect(page).to have_content("ordered")
-    expect(page).to have_content(order.created_at)
-    expect(page).to have_content(order.total)
-  end
+      visit admin_orders_dashboard_path
+    end
 
-  it "shows total number of orders by status" do
-    visit admin_orders_dashboard_path
-    expect(page).to have_content("Ordered 1")
+    it "shows total number of orders by status" do
+      expect(page).to have_content("Ordered 1")
+    end
+
+    it "shows accurate amount of orders by status" do
+      click_link_or_button("2")
+
+      expect(page).to have_content("Manage Paid Orders")
+    end
+
   end
 end
