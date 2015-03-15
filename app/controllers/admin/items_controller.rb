90d08a3691195eabbb669c2a5484111d0a727a92
Markus Olsen <molsen13@gmail.com>
2015-03-14 18:51:02 -0600

    Admin can create an item

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -9,4 +9,25 @@ class Admin::ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      flash[:notice] = "Item saved!"
+      redirect_to new_admin_item_path
+    else
+      flash[:notice] = "That toast already exists"
+      redirect_to new_admin_item_path
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :retired, :price)
+  end
+
 end
