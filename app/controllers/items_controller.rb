74e6636d3bf1bb9de38d9faa74968286bd2a9765
Markus Olsen <molsen13@gmail.com>
2015-03-17 15:37:16 -0600

    Move Item.find to private method

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,15 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show]
   def index
     @items = Item.where(retired: false)
   end
 
   def show
+  end
+
+  private
+
+  def set_item
     @item = Item.find(params[:id])
   end
 end
