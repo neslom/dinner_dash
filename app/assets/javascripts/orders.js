1e244afb673ec1d13e04506542e556b9bf1b92b5
Markus Olsen <molsen13@gmail.com>
2015-03-18 18:49:19 -0600

    Jquery showing an all option on dropdown filter

--- app/assets/javascripts/orders.js
+++ app/assets/javascripts/orders.js
@@ -1,20 +1,20 @@
 $(document).ready(function () {
-
   var $orders = $('.order');
-
   $('#order_filter_status').on('change', function() {
     var currentStatus = this.value;
-    var statuses = { 0: "ordered", 1: "paid", 2: "completed", 3: "cancelled" };
-    currentStatus = statuses[currentStatus]
 
-    $orders.each(function(index, order) {
-      $order = $(order);
-      if ($order.data('status') == currentStatus) {
-        $order.show();
-      } else {
-        $order.hide();
-      }
-    });
+    if(currentStatus == "all") {
+      $orders.show();
+    } else {
+      $orders.each(function(index, order) {
+        $order = $(order);
+        if ($order.data('status') == currentStatus) {
+          $order.show();
+        } else {
+          $order.hide();
+        }
+      });
+    }
   });
 
 });
