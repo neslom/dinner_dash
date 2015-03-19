93113a27e294f043c62d1bd9627195927d67e28a
Markus Olsen <molsen13@gmail.com>
2015-03-19 14:37:21 -0600

    Fix tests affected by changes

--- spec/features/admin_item_management_spec.rb
+++ spec/features/admin_item_management_spec.rb
@@ -27,56 +27,59 @@ RSpec.describe "Admin Item Management" do
 
   it "cannot create an item that already exists" do
     click_link_or_button("Create Item")
-    2.times do
-      fill_in("item[name]", with: "Beer Toast")
-      fill_in("item[description]", with: "Soggy, beery toast")
-      fill_in("item[price]", with: 3.50)
-      click_link_or_button("Create Toast")
-    end
+    fill_in("item[name]", with: "Beer Toast")
+    fill_in("item[description]", with: "Soggy, beery toast")
+    fill_in("item[price]", with: 3.50)
+    click_link_or_button("Create Toast")
 
-    expect(page).to have_content("That toast already exists")
+    click_link_or_button("Create Item")
+    fill_in("item[name]", with: "Beer Toast")
+    fill_in("item[price]", with: 3.50)
+    click_link_or_button("Create Toast")
+
+    expect(page).to have_content("Description can't be blank and Name has already been taken")
   end
 
-    it "has ability to update an item" do
-      click_link_or_button("Cheese Toast")
-      expect(current_path).to eq(edit_admin_item_path(item))
-      expect(item.price).to eq(3.0)
-      fill_in("item[price]", with: 3.50)
-      click_link_or_button("Update")
-      expect(current_path).to eq(admin_items_path)
-      expect(page).to have_content("Successfully Updated")
+  it "has ability to update an item" do
+    click_link_or_button("Cheese Toast")
+    expect(current_path).to eq(edit_admin_item_path(item))
+    expect(item.price).to eq(3.0)
+    fill_in("item[price]", with: 3.50)
+    click_link_or_button("Update")
+    expect(current_path).to eq(admin_items_path)
+    expect(page).to have_content("Successfully Updated")
+  end
+
+  it "cannot update an item to match an existing item's attributes" do
+    item_2 = create(:item, name: "Nacho Toast", description: "not your toast", price: 3.00)
+    click_link_or_button("Cheese Toast")
+
+    expect(current_path).to eq(edit_admin_item_path(item))
+    expect(item.price).to eq(3.0)
+
+    fill_in("item[name]", with: "Nacho Toast")
+    fill_in("item[description]", with: "Soggy, beery toast")
+    fill_in("item[price]", with: 3.50)
+    click_link_or_button("Update")
+
+    expect(page).to have_content("Name has already been taken")
+  end
+
+  it "has the ability to activate an item" do
+    item.retired = true
+    click_link_or_button("Cheese Toast")
+    within("#false") do
+      choose("item[retired]")
     end
+    click_link_or_button("Update")
+    expect(current_path).to eq(admin_items_path)
+    expect(page).to have_content("Successfully Updated")
+  end
 
-    it "cannot update an item to match an existing item's attributes" do
-      item_2 = create(:item, name: "Nacho Toast", description: "not your toast", price: 3.00)
-      click_link_or_button("Cheese Toast")
-
-      expect(current_path).to eq(edit_admin_item_path(item))
-      expect(item.price).to eq(3.0)
-
-      fill_in("item[name]", with: "Nacho Toast")
-      fill_in("item[description]", with: "Soggy, beery toast")
-      fill_in("item[price]", with: 3.50)
-      click_link_or_button("Update")
-
-      expect(page).to have_content("Name has already been taken")
-    end
-
-    it "has the ability to activate an item" do
-      item.retired = true
-      click_link_or_button("Cheese Toast")
-      within("#false") do
-        choose("item[retired]")
-      end
-      click_link_or_button("Update")
-      expect(current_path).to eq(admin_items_path)
-      expect(page).to have_content("Successfully Updated")
-    end
-
-    it "has ability to delete an item" do
-      click_link_or_button("Cheese Toast")
-      click_link_or_button("Delete Item")
-      expect(current_path).to eq(admin_items_path)
-      expect(page).to have_content("Item Deleted")
-    end
+  it "has ability to delete an item" do
+    click_link_or_button("Cheese Toast")
+    click_link_or_button("Delete Item")
+    expect(current_path).to eq(admin_items_path)
+    expect(page).to have_content("Item Deleted")
+  end
 end
